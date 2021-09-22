904cc00558c605495143f14002eb9bde51cf3685
Osama Malik <itsosamamalik@gmail.com>
2021-09-22 11:33:49 +0530

    Removed the residues from Backend

--- backend/user.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-let userSchema = new mongoose.Schema({
-    product :String,
-    quantity : Number
-})
-
-module.exports = mongoose.model("user", userSchema);
