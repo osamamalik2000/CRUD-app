5b0f99e9b2e19cda4f37e316cd1b3f776849d688
Osama Malik <itsosamamalik@gmail.com>
2021-09-24 10:25:36 +0530

    First firebase deployment succesfull

--- crud-app/src/environments/environment.prod.ts
+++ crud-app/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebasConfig: {
+    apiKey: 'AIzaSyCuHpOUKX4gLCk_E8B3uzdr5JwIhQSwQ70',
+    authDomain: 'fir-crudapp-a18f1.firebaseapp.com',
+    databaseURL: 'https://fir-crudapp-a18f1-default-rtdb.firebaseio.com',
+    projectId: 'fir-crudapp-a18f1',
+    storageBucket: 'fir-crudapp-a18f1.appspot.com',
+    messagingSenderId: '986408017668',
+    appId: '1:986408017668:web:731c6db5230933180b7a51',
+    measurementId: 'G-2KGN8TN1GQ',
+  }
 };
